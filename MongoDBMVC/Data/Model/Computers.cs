e22cb6f082e915e51e5f5f1ad5be707cb07186df
Paweł Martosz <pawel.m476@gmail.com>
2022-05-24 18:37:10 +0200

    Dziala

--- MongoDBMVC/Data/Model/Computers.cs
+++ MongoDBMVC/Data/Model/Computers.cs
@@ -12,17 +12,25 @@ namespace Infrastructure.Model
     public class Computers
     {
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        private string FinallyId;
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id
+        {
+            get { return FinallyId; }
+            set { if (value == null) { FinallyId = ObjectId.GenerateNewId().ToString(); } else { FinallyId = value; } }
+        }
 
         [Display(Name="Computer name")]
         public string Name { get; set; }
 
         [Display(Name ="Creation year")]
         public int Year { get; set; }
-
+        
         public string ImageId { get; set; }
 
         public bool HasImage()=> !string.IsNullOrEmpty(ImageId);    
 
+        
     }
 }
